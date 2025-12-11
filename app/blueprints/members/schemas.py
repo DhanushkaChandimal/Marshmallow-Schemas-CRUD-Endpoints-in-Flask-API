6cc969b954ead8bf1aab02de1113f6357924886a
Dhanushka <dhanushkamcr@gmail.com>
2025-12-11 17:29:39 -0600

    Finalyze converting app to application factory pattern

---
+++ app/blueprints/members/schemas.py
@@ -0,0 +1,9 @@
+from app.extensions import ma
+from app.models import Member
+
+class MemberSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Member #using the SQLAlchemy model to create fields used in serialization, deserialization, and validation
+    
+member_schema =MemberSchema()
+members_schema = MemberSchema(many=True) #variant that allows for the serialization of many Users,
